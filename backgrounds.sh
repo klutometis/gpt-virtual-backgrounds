32213e1c4a7b25e59ea8e96ddb3d2df5e7e1799f
Peter Danenberg <pcd@roxygen.org>
2024-02-08 23:29:09 +0000

    Add the initial version of the script.

---
+++ backgrounds.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+set -e
+set -o pipefail
+
+trap cleanup EXIT
+
+curl -X POST "https://api.openai.com/v1/images/generations" \
+     -H "Authorization: Bearer $(cat openai-api.key)" \
+     -H "Content-Type: application/json" \
+     -d '{
+           "model": "dall-e-3",
+           "prompt": "Can you create a virtual background which combines piano, rugby and lambda calculus?",
+           "n": 1,
+           "size": "1792x1024"
+         }' | tee /dev/stderr | jq '.data[0].url' | xargs curl | convert - -resize 1920x1080 ~/Downloads/background.webp
